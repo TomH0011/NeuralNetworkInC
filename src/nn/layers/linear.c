dc7aca228615aca2a56d8888b082dc77d2c80631
Tom Howard <165574283+TomH0011@users.noreply.github.com>
2025-11-24 21:30:21 +0000

    added ReLU and GeLU

--- src/nn/layers/linear.c
+++ src/nn/layers/linear.c
@@ -1,1 +1,11 @@
 #include "../../../include/deepc/nn.h"
+#include <math.h>
+// Activation function takes data input outputs GeLU result for neurons
+float gelu(const float x) {
+     return (x / 2) * (1 + erfcf(x));
+}
+// Activation function for ReLU, piecewise and more simple than GeLU
+float relu(const float x) {
+     return (x > 0) ? x : 0;
+
+}
